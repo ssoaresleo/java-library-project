af374b4440a1e276fa760ae2b742f869b4a11fc3
Leonardo Henrique <leonardohenriquesoarasalvez784@gmail.com>
2024-08-24 17:06:58 -0300

    feat: add hash emprestimos

--- src/Cliente.java
+++ src/Cliente.java
@@ -1,30 +1,55 @@
-import java.util.Date;
+import java.util.*;
 
 public class Cliente {
     private static int contadorId = 1;
     private int id;
-    private String name;
+
+    private String nome;
     private Date dataDeNascimento;
     private String email;
 
-    public Cliente(String name, Date dataDeNascimento, String email) {
+    private Map<Cliente, List<Emprestimo>> historicoEmprestimos = new HashMap<>();
+
+    public Cliente(String nome, Date dataDeNascimento, String email) {
         this.id = contadorId++;
-        this.name = name;
+        this.nome = nome;
         this.dataDeNascimento = dataDeNascimento;
         this.email = email;
     }
 
-    public String getName() {
-        return name;
+    public String getNome() {
+        return nome;
     }
 
     public Date getDataDeNascimento() {
         return dataDeNascimento;
     }
 
+    public void adicionarEmprestimo(Emprestimo emprestimo) {
+        historicoEmprestimos.computeIfAbsent(this, k -> new ArrayList<>()).add(emprestimo);
+    }
+
+    public List<Emprestimo> buscarEmprestimos() {
+        return historicoEmprestimos.computeIfAbsent(this, k -> new ArrayList<>());
+    }
+    public void exibirEmprestimos() {
+        List<Emprestimo> emprestimos = buscarEmprestimos();
+
+        if (emprestimos.isEmpty()) {
+            System.out.println("Nenhum empréstimo encontrado para o cliente " + nome);
+        } else {
+            for (Emprestimo emprestimo : emprestimos) {
+                System.out.println(emprestimo);
+            }
+        }
+    }
+
     public String getEmail() {
         return email;
     }
 
-
+    @Override
+    public String toString() {
+        return String.format("Cliente: %s, \nEmail: %s", nome, email);
+    }
 }
